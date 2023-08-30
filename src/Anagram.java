c7ce0bc32ae6f33f8169c903b09043d832d7df86
JustChroma <30071757@nescol.ac.uk>
2023-08-30 14:57:47 +0100

    9 letter Anagrams

---
+++ src/Anagram.java
@@ -0,0 +1,14 @@
+public class Anagram {
+
+    public static void main(String[] args) {
+
+        System.out.println("Here are 5 9-letter anagrams:");
+        System.out.println("split race = Replastic");
+        System.out.println("gas in rut = Signature");
+        System.out.println("give data = Navigated");
+        System.out.println("rats sent = Resistant");
+        System.out.println("grim saint = Mastering");
+
+
+    }
+}
