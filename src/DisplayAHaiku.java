3e2b340bed7e506720deb00efee1d8afccab48a0
JustChroma <30071757@nescol.ac.uk>
2023-08-30 14:11:30 +0100

    News Haiku

---
+++ src/DisplayAHaiku.java
@@ -0,0 +1,9 @@
+public class DisplayAHaiku {
+
+    public static void main(String[] args) {
+        //Prints 'Hello World'
+        System.out.println("In shadows of cells");
+        System.out.println("Failures masked by silent cams");
+        System.out.println("Truth's weight bears heavy.");
+    }
+}
