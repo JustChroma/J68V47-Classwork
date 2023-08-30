a755ca66aed07f3fa7618715482d0c41a401d64c
JustChroma <30071757@nescol.ac.uk>
2023-08-30 13:42:10 +0100

    Hello World Program

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * My First Program
+ * @Author Findlay Pell
+ * @Version 1
+ */
+
+public class HelloWorld {
+
+    public static void main (String[] args) {
+        //Prints 'Hello World'
+        System.out.println("Hello World");
+    }
+
+
+}
